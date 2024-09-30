16093c1e5d3f719c6374dde5da883a8226261694
Diaphanous77 <olegvlasov08@gmail.com>
2024-09-30 18:02:57 +0000

    117

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
