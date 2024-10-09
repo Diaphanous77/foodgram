49ef05c6b597c7b9355064b4ed34ea008f3d57ae
Diaphanous77 <olegvlasov08@gmail.com>
2024-10-09 13:30:48 +0000

    Revert "136"
    This reverts commit 8b4e28e95599183a6dafb65d631a966a2a3f0a39.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,9 +11,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['diapfoodgram.sytes.net', '127.0.0.1', 'localhost', '158.160.2.164',]
-
 CSRF_TRUSTED_ORIGINS = ['https://diapfoodgram.sytes.net']
+
+ALLOWED_HOSTS = ['diapfoodgram.sytes.net', '127.0.0.1', 'localhost', '158.160.2.164',]
 
 INSTALLED_APPS = [
     'rest_framework',
