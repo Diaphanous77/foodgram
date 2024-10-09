ea05794309bd86d56c76214c4a6cc75297b2c618
Oleg Vlasov <olegvlasov08@gmail.com>
2024-10-09 15:09:49 +0300

    133

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -98,7 +98,9 @@ class UserPostSerializer(UserCreateSerializer):
     def create(self, validated_data):
         user = User(
             email=validated_data['email'],
-            username=validated_data['username']
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
         )
         user.set_password(validated_data['password'])
         user.save()
