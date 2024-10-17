1561c570c271375137d7ff3b50d63128adb842b6
Diaphanous77 <olegvlasov08@gmail.com>
2024-10-17 11:20:00 +0000

    148.2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -69,7 +69,7 @@ class Recipe(models.Model):
     )
     image = models.ImageField(
         'Картинка',
-        upload_to='/media_imgs/recipes/',
+        upload_to='media_imgs/recipes/',
     )
     text = models.TextField(
         'Описание рецепта',
